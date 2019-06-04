825b1e40aa4d3ead21f589eb25fba72c4e4e53fb
hufei <hufeispace@gmail.com>
2019-06-04 19:24:41 +0800

    init

---
+++ okay-conf.js
@@ -0,0 +1,101 @@
+fis.set('charset', 'utf-8');
+fis.set('namespace', "admin");
+fis.set('map_file', '/src/resource-map.js');
+
+
+fis.hook(require('../fis3-hook-npm'));
+
+
+fis.match('/**\.{css,scss}', {
+    // 图片合并
+    useSprite: true
+});
+
+fis.match('/src/{require.js, resource-map.js}', {
+    isMod: false,
+    parser: null
+});
+fis.match('::package', {
+    // 图片合并
+    spriter: fis.plugin('csssprites', {
+        // 排列方式, linear || matrix
+        layout: 'linear'
+    })
+});
+
+// 编译vue组件
+fis.match('/src/**.vue', {
+    parser: [    
+        fis.plugin('vue-component', {
+            runtimeOnly: true,
+            styleNameJoin: '',
+            extractCSS: false,
+            cssScopedIdPrefix: '_v-',
+            cssScopedHashType: 'sum',
+            cssScopedHashLength: 8,
+            cssScopedFlag: '__vuec__',
+			isPartial:false,
+        })
+		,fis.plugin('babel-7', {
+            sourceMap: 'both'
+        }) // 处理ES6
+    ]
+});
+
+// fis.match('*.{css,scss}', {
+//     // 开启图片压缩;
+//     // useSprite: true,
+//     // css 压缩;
+//     optimizer: fis.plugin('clean-css')
+//     // ,packTo: '/pkg/quizcenter.css'
+// })
+// .match('{**.js,**.vm:js,**.html:js,**.vue}', {
+//     // js 压缩;
+//     optimizer: fis.plugin('uglify-js', {})
+// });
+
+
+fis.match('/src/(**.{html, js, png, scss, css, jpg, vue})', {
+    release :'$1',
+    // deploy: fis.plugin('local-deliver', {
+    //     to: path.resolve(__dirname, './output')
+    // })
+});
+fis.match('/src/(**).vue', {
+    release :'$1_vue.js',
+    // deploy: fis.plugin('local-deliver', {
+    //     to: path.resolve(__dirname, './output')
+    // })
+});
+
+
+fis.match('/node_modules/(*)/**.js', {
+    release :'$0',
+    // deploy: fis.plugin('local-deliver', {
+    //     to: path.resolve(__dirname, './output')
+    // })
+});
+
+
+
+fis.media('112').match('/src/(**.{html, js, png, scss, css, jpg, vue})', {
+    release :'$1',
+    deploy: fis.plugin('http-push', {
+        receiver: 'http://10.60.0.112:7999/okay-upload',
+        to: '/xdfapp/www/xin.static.fe.xk12.cn'
+    })
+})
+.match('/src/(**).vue', {
+    release :'$1_vue.js',
+    deploy: fis.plugin('http-push', {
+        receiver: 'http://10.60.0.112:7999/okay-upload',
+        to: '/xdfapp/www/xin.static.fe.xk12.cn'
+    })
+})
+.match('/node_modules/**.js', {
+    release :'$0',
+    deploy: fis.plugin('http-push', {
+        receiver: 'http://10.60.0.112:7999/okay-upload',
+        to: '/xdfapp/www/xin.static.fe.xk12.cn',
+    })
+});
---
+++ src/views/login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <form action="/login" method="POST">
+            <div>
+                <label for="username">用户名：</label>
+                <input type="text" name="username" placeholder="请输入6到8位用户名">
+                <span class="tip"></span>
+            </div>
+            <div>
+                <label for="password">密码：</label>
+                <input type="password" name="password" placeholder="请输入6到8位用户名">
+                <span class="tip"></span>
+            </div>
+            <div>
+                <button type="submit">登录</button>
+            </div>
+            
+        </form>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+form {
+    label{
+        width: 25%;
+        text-align: right;
+    }
+    input{
+        width: 70;
+    }
+}
+</style>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+
+
